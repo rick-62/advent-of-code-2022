6a3a0c7a37c9dfa521346f88adedacd039136b3d
rick-62 <rick-62@hotmail.co.uk>
2022-12-03 10:38:42 +0000

    day03 part 1

--- advent_of_code_2022/day03.py
+++ advent_of_code_2022/day03.py
@@ -1,10 +1,13 @@
 from helper import load_input
+from itertools import combinations
+import string
 
 def create_input():
-    return load_input(3).readlines()
+    return load_input(3).read().replace('\n', ' ').split()
 
 
-def split_string(string: str, n_chunks=2):
+def split_into_chunks(string: str, n_chunks=2):
+    '''split string evenly into n chunks''' 
 
     len_chunk = int(len(string)/n_chunks)
 
@@ -14,16 +17,39 @@ def split_string(string: str, n_chunks=2):
     ]
 
 
-def extract_common_letter(split_string):
-    pass
+def extract_common_items(compartments):
 
-# compare strings
-# extract common letters
+    # convert strings to sets
+    sets = [set(chunk) for chunk in compartments]
+
+    # identify common letters
+    intersection = [a & b for a, b in combinations(sets, 2)]
+
+    # return distinct list of common letters
+    return ''.join(set.union(*intersection))
+
+
+
+
+def calculate_total_score(bags):
+
+    common_items = ''.join([extract_common_items(split_into_chunks(bag)) for bag in bags])
+
+    print(common_items)
+
+    return sum([string.ascii_letters.index(item) + 1 for item in common_items])
+
+
+    
+
+
+
 # prioritise & create score
 
 
 
 if __name__ == '__main__':
-    data = create_input()
-    print(split_string('234532jgfusdjgn98g'))
-
+    bags = create_input()
+    print(split_into_chunks('234532jgfjjusdjgn98g'))
+    print(extract_common_items(['234532jgf', 'jusdjgn98']))
+    print(calculate_total_score(bags))
