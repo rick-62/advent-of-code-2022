aea2f3d90f2196728d86cea8a15ce7cfddd42535
rick-62 <rick-62@hotmail.co.uk>
2022-12-05 22:59:56 +0000

    day05 complete

--- advent_of_code_2022/day05.py
+++ advent_of_code_2022/day05.py
@@ -1,38 +1,70 @@
-from helper import load_input
+import re
 
-from typing import Iterable
+from collections import defaultdict
+from typing import Iterable, Dict, List
+
+from helper import load_input
 
 
 def create_input():
     return load_input(5).read().split('\n\n')
     
 
-def initialise_stacks(raw_stacks: str) -> Dict[List[str]]:
+def initialise_stacks(raw_stacks: str) -> Dict[int, List[str]]:
     '''Convert raw text stacks to dictionary'''
-    # Load in the stacks (naive way will do here)
-    # each crate is fixed width (number of spaces)
-    # Use dictionary for each stack number Dict[List[str]]
-    # will need to get rerun if to reuse stacks (could use cache?)
-    pass
+
+    # intiialise stacks dict
+    stacks = defaultdict(list)
+
+    # split crates & separate stack ids
+    lines = raw_stacks.splitlines()
+    stack_ids = lines.pop(-1)
+
+    # Load in the stacks (naive way will do here)   
+    for stack in re.finditer(r'\d', stack_ids):
+        stacks[int(stack.group())] = [
+            line[stack.start()] for line in lines[::-1] if line[stack.start()] != ' '
+        ]
+    return stacks
 
 
-def yield_procedure(raw_procedure: str) -> Iterable[int]:
-    # loop through raw_procedures to extract values
-    pass
+def yield_procedure(raw_procedures: str) -> Iterable[int]:
+    '''yield the next procedure, extracting 3 key values'''
+    for procedure in raw_procedures.splitlines():
+        yield [int(d) for d in re.findall(r'\d+', procedure)]
 
 
-def rearrange_crates(stacks: Dict[List[str]]):
-    # for procedure in yield_procedure
-    # update stacks
-    pass
+def rearrange_crates(raw_procedures, stacks: Dict[int, List[str]]):
+    '''Rearrange crates moving 1 crate at a time, n times'''
+    for n, i1, i2 in yield_procedure(raw_procedures):
+        for _ in range(n):
+            stacks[i2].append(stacks[i1].pop())
+    return stacks
+
+
+def rearrange_crates_9001(raw_procedures, stacks: Dict[int, List[str]]):
+    '''Rearrange crates moving n crates at a time'''
+    for n, i1, i2 in yield_procedure(raw_procedures):
+        stacks[i2].extend(stacks[i1][-n:])
+        del stacks[i1][-n:]
+    return stacks
 
 
 def extract_top_crates(stack):
-    pass
+    return ''.join(v[-1] for k,v in stack.items())
 
 
 if __name__ == '__main__':
-    raw_stacks, raw_procedure = create_input()
+
+    raw_stacks, raw_procedures = create_input()
+
+    # Part 1
     stacks = initialise_stacks(raw_stacks)
-    rearrange_crates(stacks)
+    rearrange_crates(raw_procedures, stacks)
     print(extract_top_crates(stacks))
+
+    # Part 2
+    stacks = initialise_stacks(raw_stacks)
+    rearrange_crates_9001(raw_procedures, stacks)
+    print(extract_top_crates(stacks))
+
--- tests/test_day05.py
+++ tests/test_day05.py
@@ -1,2 +1,67 @@
 from advent_of_code_2022 import day05
 
+def test_create_input():
+    assert len(day05.create_input()) == 2
+
+
+test_raw_stacks = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+"""
+
+def test_initialise_stacks():
+    assert day05.initialise_stacks(test_raw_stacks)[1] == ['Z', 'N']
+    assert day05.initialise_stacks(test_raw_stacks)[2] == ['M', 'C', 'D']
+    assert day05.initialise_stacks(test_raw_stacks)[3] == ['P']
+
+
+def test_yield_procedure():
+    assert next(day05.yield_procedure('move 1 from 2 to 1')) == [1, 2, 1]
+    assert next(day05.yield_procedure('move 3 from 1 to 3')) == [3, 1, 3]
+    assert next(day05.yield_procedure('move 2 from 2 to 1')) == [2, 2, 1]
+    assert next(day05.yield_procedure('move 1 from 1 to 2')) == [1, 1, 2]
+
+    test_input = day05.yield_procedure('move 1 from 1 to 2\nmove 2 from 2 to 1')
+    assert next(test_input) == [1, 1, 2]
+    assert next(test_input) == [2, 2, 1]
+
+
+test_procedures = """move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+
+
+def test_rearrange_crates():
+    test_stacks = {
+        1: ['Z', 'N'],
+        2: ['M', 'C', 'D'],
+        3: ['P'],
+    }
+    assert day05.rearrange_crates(test_procedures, test_stacks) == {
+        1: ['C'],
+        2: ['M'],
+        3: ['P', 'D', 'N', 'Z'],
+    }
+
+
+def test_extract_top_crates():
+    assert day05.extract_top_crates({
+        1: ['C'],
+        2: ['M'],
+        3: ['P', 'D', 'N', 'Z'],
+    }) == 'CMZ'
+
+
+def test_rearrange_crates_9001():
+    test_stacks = {
+        1: ['Z', 'N'],
+        2: ['M', 'C', 'D'],
+        3: ['P'],
+    }
+    assert day05.rearrange_crates_9001(test_procedures, test_stacks) == {
+        1: ['M'],
+        2: ['C'],
+        3: ['P', 'Z', 'N', 'D'],
+    }
