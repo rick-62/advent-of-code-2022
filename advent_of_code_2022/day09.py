19daa7cd34e9c7718531cb90da6b5b9de5567646
rick-62 <rick-62@hotmail.co.uk>
2022-12-10 09:56:56 +0000

    day09 in progress

--- advent_of_code_2022/day09.py
+++ advent_of_code_2022/day09.py
@@ -1,5 +1,8 @@
 
+from dataclasses import dataclass
 from typing import Tuple
+
+import numpy as np
 
 from helper import load_input
 
@@ -8,15 +11,80 @@ def create_input():
     return load_input(9).read().splitlines()
 
 
-def move_tail_coords(h_coord: Tuple[int], t_coord: Tuple[int]) -> (Tuple[int], set[Tuple[int]]):
-    '''
-    Given the new Head coords, move Tail coords accordingly.
-    Returns set of visited coordinates & final position of Tail.
-    '''
-    pass
+@dataclass
+class Tail:
+    curr_loc = np.array((0, 0))
+    visited = [np.array((0, 0))]
 
-# Ue objects instead? Tidier than moving coords around etc
-# given both coorinates, return list of coordinates Tail has followed
+    @classmethod
+    def count_visited(cls):
+        return len(set(cls.visited))
+
+    @staticmethod
+    def in_proximity(coord1, coord2):
+        i, j = coord1 - coord2
+        if abs(i) <= 1 and abs(j) <= 1:
+            return True
+        else: 
+            return False
+
+
+    @classmethod
+    def move(cls, target):
+        # check if in proximity - do nothing if they are
+        if cls.in_proximity(cls.curr_loc, target):
+            return cls.curr_loc
+                
+        # check if difference is diagonal - both abs i & j != 0
+        i, j = target - cls.curr_loc
+        if i != 0 and j != 0:
+            cls.curr_loc += np.sign((i, j))
+            cls.visited.append(cls.curr_loc)
+
+        # keep moving T until H reached, updating visited and current coord (maybe batch)
+        
+
+        return cls.curr_loc
+    
+        
+        
+
+
+@dataclass
+class Head:
+    curr_loc = np.array((0, 0))
+
+    UDLR = {
+        'U': np.array((0, 1)), 
+        'D': np.array((0, -1)), 
+        'L': np.array((-1, 0)), 
+        'R': np.array((1,0))
+    }
+
+    @classmethod
+    def move(cls, direction, value):
+        cls.curr_loc += cls.UDLR[direction] * value
+
+    
+
+    
+
+
+
+    
+
+    
+
+
+
+# def move_tail_coords(h_coord: Tuple[int], t_coord: Tuple[int]) -> (Tuple[int], set[Tuple[int]]):
+#     '''
+#     Given the new Head coords, move Tail coords accordingly.
+#     Returns set of visited coordinates & final position of Tail.
+#     '''
+#     pass
+
+# given both coordinates, return list of coordinates Tail has followed
 # for instruction, move H -> update coord, update Tail coords
     # add to set all T coordss
 # finally count the length of the set to get puzzle answer
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -1,9 +1,65 @@
 
+import numpy as np
+
 from advent_of_code_2022 import day09
 
 
 def test_create_input():
     assert len(day09.create_input()) > 5
 
+
+def test_Head():
     
+    day09.Head.move('R', 4)
+    assert list(day09.Head.curr_loc) == [4,0]
+
+    day09.Head.move('U', 4)
+    assert list(day09.Head.curr_loc) == [4,4]
+
+    day09.Head.move('L', 3)
+    assert list(day09.Head.curr_loc) == [1,4]
+
+    day09.Head.move('D', 1)
+    assert list(day09.Head.curr_loc) == [1,3]
+
+    day09.Head.move('R', 4)
+    day09.Head.move('D', 1)
+    day09.Head.move('L', 5)
+    day09.Head.move('R', 2)
+    assert list(day09.Head.curr_loc) == [2,2]
+
+
+def test_Tail():
+
+    assert list(day09.Tail.move((1, 1))) == [0, 0]
+    assert list(day09.Tail.move((1, 2))) == [1, 1]
+    assert list(day09.Tail.move((1, 3))) == [1, 2]
+
+
+    # Test count of visited
+    day09.Tail.visited = []
+    assert day09.Tail.count_visited() == 0
+    day09.Tail.visited = [(0, 0),(1, 0),(0, 5),(2, 0),(0, 0),]
+    assert day09.Tail.count_visited() == 4
+
+
+def test_in_proximity_method():
+
+    # positive coords
+    assert day09.Tail.in_proximity(np.array((0, 0)), np.array((1, 3))) == False
+    assert day09.Tail.in_proximity(np.array((1, 1)), np.array((1, 3))) == False
+    assert day09.Tail.in_proximity(np.array((1, 4)), np.array((1, 3))) == True
+    assert day09.Tail.in_proximity(np.array((2, 4)), np.array((1, 3))) == True
+
+    # negative coords
+    assert day09.Tail.in_proximity(np.array((2, 4)), np.array((-1, -3))) == False
+    assert day09.Tail.in_proximity(np.array((-2, -4)), np.array((-1, -3))) == True
+    assert day09.Tail.in_proximity(np.array((0, -3)), np.array((-1, -3))) == True
+
+
+
+
+
+
+
 
